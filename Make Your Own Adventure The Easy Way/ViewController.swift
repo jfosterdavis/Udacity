76cf36d61059e739878dfe3f39bed2e17a0b379d
Jacob Foster Davis <Snake@minisnakebite.local>
2016-08-21 22:44:54 -0400

    Initial Commit

---
+++ Make Your Own Adventure The Easy Way/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Make Your Own Adventure The Easy Way
+//
+//  Created by Jacob Foster Davis on 8/21/16.
+//  Copyright © 2016 Zero Mu, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

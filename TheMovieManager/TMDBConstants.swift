136f38654a88240cbc9ba2326f00502097e0769f
Jacob Foster Davis <jfosterdavis@users.noreply.github.com>
2016-08-31 02:19:18 -0400

    Added secrets.swift and hid.

--- TheMovieManager/TMDBConstants.swift
+++ TheMovieManager/TMDBConstants.swift
@@ -14,7 +14,7 @@ extension TMDBClient {
     struct Constants {
         
         // MARK: API Key
-        static let ApiKey : String = "YOUR_API_KEY_HERE"
+        static let ApiKey : String = Secrets.APIKey
                         
         // MARK: URLs
         static let ApiScheme = "https"
